9d9d848583808378845be403660ba1ce5a338b95
Steve Ford <sford@geeky-boy.com>
2025-11-03 12:18:35 -0500

    updates

--- wsl_fixcore.c
+++ wsl_fixcore.c
@@ -1,15 +1,16 @@
 /* wsl_fixcore.c - program to reset /proc/sys/kernel/core_pattern to "core.%p"
  * so you get traditional core dumps. For full doc and source, see
  * https://github.com/fordsfords/wsl_fixcore */
-/*
-# This code and its documentation is Copyright 2020 Steven Ford, http://geeky-boy.com
-# and licensed "public domain" style under Creative Commons "CC0": http://creativecommons.org/publicdomain/zero/1.0/
-# To the extent possible under law, the contributors to this project have
-# waived all copyright and related or neighboring rights to this work.
-# In other words, you can use this code for any purpose without any
-# restrictions.  This work is published from: United States.  The project home
-# is https://github.com/fordsfords/wsl_fixcore
-*/
+
+/* This work is dedicated to the public domain under CC0 1.0 Universal:
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * 
+ * To the extent possible under law, Steven Ford has waived all copyright
+ * and related or neighboring rights to this work. In other words, you can 
+ * use this code for any purpose without any restrictions.
+ * This work is published from: United States.
+ * Project home: https://github.com/fordsfords/wsl_fixcore
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
